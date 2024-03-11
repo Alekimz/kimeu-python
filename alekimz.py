c5086c97b128b44bbe96bb8ac908be32a1a5cf2c
alekimz <muriithialex84@gmail.com>
2024-03-11 06:50:30 -0400

    more python info

---
+++ alekimz.py
@@ -0,0 +1,29 @@
+#variables
+student_name ="alex" #data type is string
+student_age ="23" #data type is integer
+student_marks ="65" #data type is integer
+is_male= True #data type is boolean
+student_fee =100.0 #data type is float
+
+#outputting the values in the variables
+print(student_name)
+print(student_age)
+print(is_male)
+
+student_name ="alex"
+student_name ="ann"
+print(student_name)
+
+#case sensitivity
+STUDENT_NAME ="Mercy"
+student_name ="Kelvin"
+print(STUDENT_NAME)
+
+
+#variable naming in python
+book_price=20.0 #valid variable name
+_book_price=20.0 #valid variable name
+
+
+x=y=z=10 #assigning one value to multiple variables
+x,y,z=30,40,50 #multiple values being assigned to multiple variables
